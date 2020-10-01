d16caa988117e79c2c09ebedf43d3cd58f1a38b4
Sitaram Rathi <57089237+srrathi@users.noreply.github.com>
2020-10-02 02:50:33 +0530

    Update array.cpp

--- array.cpp
+++ array.cpp
@@ -4,9 +4,16 @@ using namespace std;
 int main()
 {
 	int size,n;
-	cout<<"Enter the size of array";
+	cout<<"Enter the size of array"<<endl;
 	cin>>size;
 	int arr[size];
-	cout<<"Enter the values of the array";
+	cout<<"Enter the values of the array"<<endl;
+	for(int i=0;i<size;i++){
+		cin>>arr[i];
+	}
+	
+	for(int i=0;i<size;i++){
+		cout<<arr[i]<<",";
+	}
 	return 0;
 }
